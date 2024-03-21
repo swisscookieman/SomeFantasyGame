a60705a4149df3fd643b40ec49c6d9da704276ac
swisscookieman <hvandoorn@bluewin.ch>
2024-03-21 10:39:59 +0100

    better printer- prints everything in a dir

--- terminal_printer.py
+++ terminal_printer.py
@@ -1,12 +1,15 @@
 import climage
 from PIL import Image
 import numpy as np
-# converts the image to print in terminal
-# inform of ANSI Escape codes
-image_path = "assets/items/iron_sword.png"
+import os
 
-img = Image.open(image_path).convert('RGB').resize((16, 16))
-arr = np.array(img)
-output = climage.convert_array(arr, is_unicode=True)
-# prints output on console.
-print(output)
+dir = "assets/items/"
+allfiles = os.listdir(dir)
+
+for image_path in allfiles:
+    image_path = dir+image_path
+    if os.path.isfile(image_path):
+        img = Image.open(image_path).convert('RGB').resize((16, 16))
+        arr = np.array(img)
+        output = climage.convert_array(arr, is_unicode=True)
+        print(output)
