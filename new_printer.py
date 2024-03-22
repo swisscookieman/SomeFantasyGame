2729ede6b2c856d346a9d9abb528d70508fc4b8d
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2024-03-22 11:57:02 +0100

    refactor plus WIP framework

--- new_printer.py
+++ new_printer.py
@@ -1,27 +1,18 @@
 from PIL import Image
-
+assets_dir = "assets/items/equipment"
 
 def print_image(image_path):
     try:
-        # Open the image file
+        # Open, resize image
         image = Image.open(image_path)
-
-        # Resize the image to fit the terminal
         image = image.resize((16, 16))
-
-        # Get the width and height of the resized image
         width, height = image.size
 
         # Iterate over each pixel and print the corresponding character with color
         for y in range(height):
             for x in range(width):
-                # Get the RGB values of the pixel
                 r, g, b, _ = image.getpixel((x, y))
-
-                # Convert RGB to ANSI color codes
-                color_code = f"\033[38;2;{r};{g};{b}m"
-
-                # Print a colored character
+                color_code = f"\033[38;2;{r};{g};{b}m" # Convert RGB to ANSI color codes
                 print(color_code + "██", end="")
 
             # Move to the next line after each row is printed
@@ -30,8 +21,13 @@ def print_image(image_path):
     except Exception as e:
         print("Error:", e)
 
+def images_list(initial_dir):
+    image_paths = []
+    for image in initial_dir:
+        image_paths.append(image)
+    return image_paths
 
-# We need to better this by making a function to do this vv
-image_paths = ["assets/items/archer_bow.png","assets/items/assasin_dagger.png","assets/items/mage_cape.png","assets/items/mage_staff.png","assets/items/mercenary_armor.png","assets/items/mercenary_axe.png","assets/items/warrior_chestplate.png","assets/items/warrior_sword.png"]
-for image in image_paths:
-    print_image(image)
+image_paths = images_list(assets_dir)
+print(image_paths)
+# for image in image_paths:
+#     print_image(image)
