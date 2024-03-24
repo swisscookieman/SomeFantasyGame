3b0a36c9b0d082303c0f7c5ad3ee73658bcd1ea2
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2024-03-24 12:11:58 +0100

    refactor

--- save_handler.py
+++ save_handler.py
@@ -1,8 +1,10 @@
-import json
 import image_printer as image_printer
+import printing_utility as pu
+import json
 
+savepath = "local/save.json"
 
-def load_save(savepath):
+def load(savepath):
     f = open(savepath)
     playersave = json.load(f)
 
@@ -12,11 +14,11 @@ def load_save(savepath):
         print("\n----------------------------------------------------------------------\n")
         item_details = playersave['owned_items'][item]
         print(
-            f"{item_details['amount']} {item_details['itemname']} with id {item_details['itemid']} of rarity \033[92m{item_details['rarity']}\033[0m")
+            f"{item_details['amount']} {item_details['itemname']} with id {item_details['itemid']} of rarity {pu.Colors.green}{item_details['rarity']}{pu.Colors.reset}")
         print(
-            f"Item is currently enchanted with {item_details['enchant1']}, {item_details['enchant2']}, {item_details['enchant3']}")
+            f"Item is currently enchanted with {item_details['enchant1']}, {item_details['enchant2']}, {item_details['enchant3']}\n")
         imagedir = "assets/items/equipment/" + item_details['itemid'] + ".png"
         image_printer.print_image(imagedir)
 
-
-load_save("local/save.json")
+def new(savepath): # Needs to overwrite any save to a fresh one. Could copy from a template that is in the github (local_save will be in gitignore)
+    pass
