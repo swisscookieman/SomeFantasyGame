36f32c8cd661fb1fa3088e66a1dc1b18f84de5eb
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2024-03-24 12:11:29 +0100

    added new/load save system to main not to handler

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import printing_utility as pu
+import save_handler as save
 import traceback
 import logging
 
@@ -7,14 +8,16 @@ def main():
     pu.clear_terminal()
     pu.print_game_title()
     print("Welcome to SomeFantasyGame ! Would you like to load your save ?\n\n")
-    user_input = input("\033[38;2;0;0;255mwajwdn\033[0m")
+    user_input = input(f"{pu.Colors.input_blue}[1]{pu.Colors.reset} - Load\n{pu.Colors.input_blue}[2]{pu.Colors.reset} - New Save\n{pu.Colors.white_b}Input - {pu.Colors.reset}")
+    pu.clear_terminal()
     match user_input:
-        case 1:
-            pass
-        case 2:
-            pass
+        case "1":
+            save.load(save.savepath)
+        case "2":
+            save.new(save.savepath)
         case _ :
-            pass
+            print(f"{pu.Colors.warning_yellow}Loading save by default{pu.Colors.reset}") # We may have to redo this
+            save.load(save.savepath)
 
 if __name__ == "__main__":
     try:
