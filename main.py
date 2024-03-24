1a38982f38deb37f093835ba62c40aadc807c81b
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2024-03-24 11:09:39 +0100

    WIP main save load

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-import logging
-import traceback
 import printing_utility as pu
+import traceback
+import logging
 
 
 def main():
+    pu.clear_terminal()
     pu.print_game_title()
-    
-
-
+    print("Welcome to SomeFantasyGame ! Would you like to load your save ?\n\n")
+    user_input = input("\033[38;2;0;0;255mwajwdn\033[0m")
+    match user_input:
+        case 1:
+            pass
+        case 2:
+            pass
+        case _ :
+            pass
 
 if __name__ == "__main__":
     try:
