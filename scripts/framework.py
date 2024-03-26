f01d1f3a1997c5fe23c9155b2c905483044ab74d
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2024-03-26 12:23:39 +0100

    WIP main menu

--- scripts/framework.py
+++ scripts/framework.py
@@ -1,4 +1,23 @@
-import printing_utility as pu
+import scripts.printing_utility as pu
+import scripts.save_handler as save
+import time
+
+def start():
+    pu.clear_terminal()
+    pu.print_game_title()
+    print("Welcome to SomeFantasyGame ! Would you like to load your save ?\n\n")
+    user_input = input(f"{pu.Colors.input_blue}[1]{pu.Colors.reset} - Load\n{pu.Colors.input_blue}[2]{pu.Colors.reset} - New Save\n{pu.Colors.white_b}Input - {pu.Colors.reset}")
+    pu.clear_terminal()
+    if user_input == "2":
+        save.new(save.savetemplate_path,save.save_path)
+    print(f"{pu.Colors.warning_yellow}Starting game.{pu.Colors.reset}")
+    time.sleep(0.25)
+    pu.clear_terminal()
+    print(f"{pu.Colors.warning_yellow}Starting game..{pu.Colors.reset}")
+    time.sleep(0.25)
+    pu.clear_terminal()
+    print(f"{pu.Colors.warning_yellow}Starting game...{pu.Colors.reset}")
 
 def main_menu():
-    pass
+    pu.clear_terminal()
+    pu.print_game_title()
