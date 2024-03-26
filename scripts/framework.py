4882f3882dfca013f40a3c31648f63fecf1f4366
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2024-03-26 08:20:53 +0100

    WIP framework

--- scripts/framework.py
+++ scripts/framework.py
@@ -1,15 +1,4 @@
-class Monster:
-    def __init__(self,max_health,defense,speed,damage,crit_chance):
-        self.max_health = max_health
-        self.defense = defense
-        self.speed = speed
-        self.damage = damage
-        self.crit_chance = crit_chance
-        self.health = self.max_health
+import printing_utility as pu
 
-    def attack(self):
-        #todo player.health -= self.damage
-        print(f"Dealt {self.damage} damage")
-
-    def heal(self,heal_amount:float):
-        self.health += self.max_health * heal_amount # Heals by a percentage of max health
+def main_menu():
+    pass
