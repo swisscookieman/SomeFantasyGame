4a0761fb60dc496bf90a57fcc8459de65768661e
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2024-03-26 12:39:18 +0100

    WIP other menus

--- scripts/framework.py
+++ scripts/framework.py
@@ -22,5 +22,17 @@ def main_menu():
     pu.clear_terminal()
     pu.print_game_title()
     data = save.get_data()
-    print(f"Welcome back {data['playername']}!")
+    print(f"Welcome back {data['playername']}!\n")
     user_input = str(input(f"{pu.Colors.input_blue}[1]{pu.Colors.reset} - Fight\n{pu.Colors.input_blue}[2]{pu.Colors.reset} - Inventory\n{pu.Colors.input_blue}[3]{pu.Colors.reset} - Craft\n{pu.Colors.input_blue}[4]{pu.Colors.reset} - Stats\n{pu.Colors.white_b}Input - {pu.Colors.reset}"))
+
+def monsters():
+    pu.clear_terminal()
+    pu.print_monsters_title()
+
+def inventory():
+    pu.clear_terminal()
+    pu.print_inventory_title()
+
+def crafting():
+    pu.clear_terminal()
+    pu.print_crafting_title()
